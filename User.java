e5e78942017990dbf490f51f161122f9293f21ab
kevinn-chan <chankangle.kevin@gmail.com>
2025-10-21 16:04:20 +0800

    Update User.java

--- User.java
+++ User.java
@@ -3,15 +3,15 @@ package Internship_project;
 public abstract class User {
 	
 	private String userID;	// unique identifier of user
-	
-	private String name;	// full name of user
-	
+	private String name;	// full name of user	
 	private String password;	//user's password
+	private filtering filters;
 	
 	public User(String userID, String name, String password) {
 		this.userID = userID;
 		this.name = name;
-		this.password = password;}
+		this.password = password;
+		this.filters = new filtering();}		//each user gets their own filter options
 	
 	public String getUserID() {
 		return userID;}		// gets users unique ID
@@ -19,43 +19,31 @@ public abstract class User {
 	public String getName() {
 		return name;}		// gets users name
 	
-	public String getPassword() {
-		return password;}	// gets users password
+	/*private String getPassword() {
+		return password;}*/	// gets users password, BUT SHOULD NOT BE PUBLIC MIGHT REMOVE
+	
+	public filtering getFilters() {
+		return filters;}
 	
 	public boolean changePassword(String oldPassword, String newPassword) {
 		if (!(this.password.equals(oldPassword))) {
 			System.out.println("Incorrect password entered.");
 			return false;}
-		else {
-			if (newPassword.isEmpty()) {
-				System.out.println("New password cannot be empty.")
+		
+		if (newPassword.isEmpty()) {
+				System.out.println("New password cannot be empty.");
 				return false;}
 			
-			if (this.password.equals(oldPassword)) {
+		if (this.password.equals(oldPassword)) {
 			this.password = newPassword;
 			System.out.println("Password successfully changed.");
-			return true;}}		//method to change password
-		}
-
-
-//should we change the password here? then we wld be mixing ui side and code? 
-/**
- * public void changePassword(Scanner sc){
- * 		System.out.println("Enter Password: "
- * 		String oldPassword = sc.next();
- * 		if (this.password == oldPassword){
- * 			System.out.print("Enter new password: ");
- * 			String newPassword = sc.next();
- * 			this.password = newPassword;
- * 			System.out.println("Password changed!");}
- * 		else{
- * 			System.out.println("Incorrect Password.");
- */
-	
+			return true;}		//method to change password
+		
+		return false;}		//default
 
 
 	public boolean checkPassword(String password) {
-		return this.password.equals(password);} 	//if password is correct
+		return this.password.equals(password);} 	//if password is correct, used for encapsulation
 
 	public abstract void showMenu();		// to be defined in all subclasses
 }
