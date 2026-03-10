50fda500fa4548764148d3456d7b86a1adb3b53d
Kevin Chan <chankangle.kevin@gmail.com>
2026-03-10 13:30:04 +0800

    Update CompanyRep.java

--- CompanyRep.java
+++ CompanyRep.java
@@ -1,23 +1,23 @@
-package Internship_project;
+package internship_project;
 
-import Internship_project.enums.RepStatus;
+import internship_project.enums.RepStatus;
 
 public class CompanyRep extends User {
 		
-	private final String companyname;
+	private final String companyName;
 	private final String department;
 	private final String position;
 	private RepStatus status;
 	
-	public CompanyRep(String userID, String name, String password, String companyname, String department, String position ) {
+	public CompanyRep(String userID, String name, String password, String companyName, String department, String position ) {
 		super(userID, name, password);
-		this.companyname = companyname;
+		this.companyName = companyName;
 		this.department = department;
 		this.position = position;
-		this.status = RepStatus.PENDING;}	//autoset default pending,  need approval
+		this.status = RepStatus.PENDING;}	//autoset default to pending,  need approval from career staff
 	
 	public String getCompanyName() {
-		return companyname;}
+		return companyName;}
 	
 	public String getDepartment() {
 		return department;}
@@ -32,26 +32,17 @@ public class CompanyRep extends User {
 		this.status = status;}
 		
 	public void showMenu() {
-		System.out.println("Company Representative Menu: ");
-		System.out.println("-".repeat(50));
-		System.out.println("Hello" + super.getName() + "," + getCompanyName() + "!");
-		System.out.println("-".repeat(50));
+		System.out.println("\nCompany Representative Menu: ");
+		System.out.println("-".repeat(40));
+		System.out.println("Hello " + super.getName() + " (" + getCompanyName() + ")!");
+		System.out.println("-".repeat(40));
 		System.out.println("What would you like to do?");
 		System.out.println("1. Create Internship Opportunities");
 		System.out.println("2. Manage My Internships");
 		System.out.println("3. Manage Applications For My Internships");
-		System.out.println("4. Toggle Visibility");
+        System.out.println("4. Set View Filters");
 		System.out.println("5. Change Password");
 		System.out.println("6. Logout");
+		System.out.println("-".repeat(40));
 	}
 }
-
-		
-	
-	
-		
-	
-		
-
-	
-
